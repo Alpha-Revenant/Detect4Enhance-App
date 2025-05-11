e67515058b7a55f8dbafdd1242de2d6f256c72e0
Alpha-Revenant <160203879+Alpha-Revenant@users.noreply.github.com>
2025-05-10 20:18:48 -0700

    Add frontend folder

---
+++ detect4enhance-frontend/LiveFaceDetectionApp-main/src/screens/aboutus/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function AboutUs() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>AboutUs</Text>
+        </View>
+    );
+}
